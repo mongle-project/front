90e3a01912ccc1c42952746ea2599cbc90b76671
eunyoung-study <eunyoung.study@gmail.com>
2025-12-16 19:03:30 +0900

    git 테스트

--- client/src/pages/home/DashboardPage.jsx
+++ client/src/pages/home/DashboardPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const DashboardPage = () => {
   return (
     <div className="dashboard-page">
       <h1>대시보드</h1>
-      <p>내 반려동물 관리</p>
+      <p>내 반려동물 관리 !!!</p>
     </div>
   );
 };
